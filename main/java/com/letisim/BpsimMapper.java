9d8e83d7984bcb7a2d0352f2af6e90e8488ab216
Иван Салухов <ivansalukhov@MacBook-Air.local>
2026-05-08 17:46:48 +0300

    Refactor project to standalone BPSim parser library

--- main/java/com/letisim/BpsimMapper.java
+++
@@ -1,8 +0,0 @@
-package com.letisim;
-
-import com.letisim.dto.LetisimScenarioDto;
-import org.bpsim.model.Scenario;
-
-public interface BpsimMapper {
-    LetisimScenarioDto mapToDto(Scenario bpsimScenario);
-}
